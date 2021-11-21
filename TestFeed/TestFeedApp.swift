0b1d0fdac4e1c9501af7d8fc62924c145f7e9aa1
IlMatveev <ilya.matveev@ripecode.dev>
2021-11-21 22:46:55 +0300

    Initial Commit

---
+++ TestFeed/TestFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFeedApp.swift
+//  TestFeed
+//
+//  Created by Ilya Matveev on 21.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
